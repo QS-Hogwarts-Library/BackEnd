3615548efc2c4bbe41f569458a3afc2622399edd
Niascreate <lavi190505@gmail.com>
2026-05-12 23:32:14 -0300

    configuração inicial do Spring Security

--- src/test/java/com/example/educationalqualityproject/controller/BookControllerTest.java
+++ src/test/java/com/example/educationalqualityproject/controller/BookControllerTest.java
@@ -1,20 +1,25 @@
 package com.example.educationalqualityproject.controller;
 
-import com.example.educationalqualityproject.entity.Book;
-import com.example.educationalqualityproject.service.BookService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import com.example.educationalqualityproject.entity.Book;
+import com.example.educationalqualityproject.service.BookService;
 
 @Controller
 @RequestMapping("/books")
-public class BookController {
+public class BookControllerTest {
 
-    @Autowired
-    private BookService bookService;
+@Autowired
+private BookService bookService;
 
     @GetMapping
     public String listBooks(Model model) {
